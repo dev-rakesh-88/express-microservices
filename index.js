6d946beb224808ce7d111bb3734f247d388e6443
dev-rakesh-88 <dev.rakeshkrishnan@gmail.com>
2023-07-20 12:03:16 +0530

    second folder

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const services = require('./services.json')
+const ENV = process.env
+const PORT = ENV.PORT
+const app = express()
+const appInit = () => {
+    console.log('Server started at PORT'.replace('PORT', PORT))
+}
+app.all('/:name',async (req,res)=> {
+    let port = services[req.params.name].port
+    let config = {
+        method: req.method,
+        maxBodyLength: Infinity,
+        url: 'http://127.0.0.1:port'.replace('port',port),
+        headers: req.headers
+      };
+      
+      const response = await axios.request(config)
+      res.send(response.data)
+})
+app.listen(PORT, appInit())
