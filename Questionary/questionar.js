f8b87a3e6f14b45b2433832889b32042b21fcc1b
dev-rakesh-88 <dev.rakeshkrishnan@gmail.com>
2023-07-20 13:15:27 +0530

    added port

--- Questionary/questionar.js
+++ Questionary/questionar.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const ENV = process.env
-const PORT = ENV.PORT
+const PORT = ENV.PORT || 3002
 const app = express()
 app.get('/', (req, res)=> {
     res.send('message from questionary')
